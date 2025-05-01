a1d1c0e4f8d2714896f0c416db98a9e95a90e783
ZippyGorilla <54118929+ZippyGorilla@users.noreply.github.com>
2025-05-02 00:55:45 +0200

    visual adaptation working!
    ND_Sound tag assigned to ' ND Sound ' game object

--- Assets/XRI_Examples/AdamAmbientZoneScripts/VisualSettings.cs
+++ Assets/XRI_Examples/AdamAmbientZoneScripts/VisualSettings.cs
@@ -10,17 +10,17 @@ public class VisualSettings : MonoBehaviour
     private void Start() {
         //SetVolume(PlayerPrefs.GetFloat("SavedMasterVolume, 0.0")); 
         darkOverlayG = GameObject.FindGameObjectWithTag("ND_Sound").GetComponent<DarknessOverlayGradient>();
-        SetVal(0);
+        SetVal(100);
     }
 
     public void SetVal(float _value) { 
-        if(_value < 1) { //* needed?
-            _value = .001f;
-        }
+        //if(_value < 1) { //* needed?
+            //_value = .001f;
+        //}
 
         RefreshSlider(_value);
         //darkness slider here based off of -->* masterMixer.SetFloat("MasterVolume", Mathf.Log10(_value / 100) * 20f);   
-        //darkOverlayG.SetZoneDarkness(_value);
+        darkOverlayG.SetZoneDarkness(_value);
     }
 
     public void SetValFromSlider() {
