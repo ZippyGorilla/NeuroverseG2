7140e0a115dcf52e620897c58473bb6a589956c3
ZippyGorilla <54118929+ZippyGorilla@users.noreply.github.com>
2025-05-05 14:07:03 +0200

    visual smoothing working

--- Assets/XRI_Examples/AdamAmbientZoneScripts/VisualSettings.cs
+++ Assets/XRI_Examples/AdamAmbientZoneScripts/VisualSettings.cs
@@ -5,19 +5,34 @@ using UnityEngine.UI;
 public class VisualSettings : MonoBehaviour
 {
     [SerializeField] Slider visSlider;
-
     DarknessOverlayGradient darkOverlayG;
+    private float smoothedValue = 0f;
+    private float lastValue = 0f;
+    private float timeToSettle = 0.15f; // desired delay in seconds
+    private float velocity = 0f; //SmoothDamp
+
     private void Start() {
-        //SetVolume(PlayerPrefs.GetFloat("SavedMasterVolume, 0.0")); 
         darkOverlayG = GameObject.FindGameObjectWithTag("ND_Sound").GetComponent<DarknessOverlayGradient>();
         SetVal(75); //Out of 100.
     }
 
+    void Update () { 
+        // Always read the latest slider position as target (lastValue)
+        lastValue = visSlider.value;
+        
+        // SmoothDamp gives eased interpolation toward lastValue
+        smoothedValue = Mathf.SmoothDamp(smoothedValue, lastValue, ref velocity, timeToSettle);
+
+        Debug.Log("brightSmooth: " + smoothedValue);
+
+         // Apply to system
+        darkOverlayG.SetZoneDarkness(smoothedValue/100f);
+    }
+
     public void SetVal(float _value) { 
 
-        RefreshSlider(_value);
-        //darkness slider here based off of -->* masterMixer.SetFloat("MasterVolume", Mathf.Log10(_value / 100) * 20f);   
-        darkOverlayG.SetZoneDarkness(_value/100f);
+        RefreshSlider(_value);  
+        //darkOverlayG.SetZoneDarkness(_value/100f);
     }
 
     public void SetValFromSlider() {
--- Assets/XRI_Examples/Scripts/SoundSettings.cs
+++ Assets/XRI_Examples/Scripts/SoundSettings.cs
@@ -7,18 +7,12 @@ public class SoundSettings : MonoBehaviour
     [SerializeField] Slider soundSlider;
     [SerializeField] AudioMixer masterMixer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
-    //vvv Euler Smoothing Algorithms variables vvv
-    public float minAlpha = 0.05f;
-    public float maxAlpha = 0.3f;
-    public float maxSpeed = 2.0f;
-
     private float smoothedValue = 0f;
     private float lastValue = 0f;
     private float timeToSettle = 0.15f; // desired delay in seconds
     private float velocity = 0f; //SmoothDamp
 
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start() {
         SetVolume(PlayerPrefs.GetFloat("SavedMasterVolume, 0.0")); 
         SetVolume(0);
@@ -53,26 +47,6 @@ public class SoundSettings : MonoBehaviour
         PlayerPrefs.SetFloat("SavedMasterVolume", smoothedValue);
         masterMixer.SetFloat("MasterVolume", Mathf.Log10(smoothedValue / 100) * 20f);
     }
-
-    void aUpdate() {
-        // Always read the latest slider position as target (lastValue)
-        lastValue = soundSlider.value;
-
-        //smoothing toward target
-        float alpha = 1f - Mathf.Exp(-Time.deltaTime / timeToSettle);
-        smoothedValue += alpha * (lastValue - smoothedValue);
-
-        // Snap when close to prevent infinite drift
-        if (Mathf.Abs(lastValue - smoothedValue) < 0.001f) smoothedValue = lastValue;
-
-        Debug.Log("UpdateSmooth: " + smoothedValue);
-
-        // Apply to system
-        PlayerPrefs.SetFloat("SavedMasterVolume", smoothedValue);
-        masterMixer.SetFloat("MasterVolume", Mathf.Log10(smoothedValue / 100) * 20f);
-    }
-
-
 
     public void QuarterVol() { //Set the Master volume slider to its mid-point.
         SetVolume(25);
