5271e75b7b79be672d009052b41f41d2026909f8
ZippyGorilla <54118929+ZippyGorilla@users.noreply.github.com>
2025-05-19 17:17:10 +0200

    logging

--- Assets/XRI_Examples/Scripts/MuteAudio.cs
+++ Assets/XRI_Examples/Scripts/MuteAudio.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+workaround = 1;
 public class MuteAudio : MonoBehaviour
 {
     // Start is called before the first frame update
     public void MuteToggle(bool muted)
     {
-        if (muted) 
+        workaround = workaround * -1;
+        if (workaround == 1)
         {
             AudioListener.volume = 0;
+
+            Debug.Log("UnityDebug Mute_Audio: " + "Muted");
         }
-        else
+        else if (workaround == -1)
         {
             AudioListener.volume = 1;
+            Debug.Log("UnityDebug Mute_Audio: " + "Unmuted");
         }
     }
 }
--- Assets/XRI_Examples/Scripts/SoundSettings.cs
+++ Assets/XRI_Examples/Scripts/SoundSettings.cs
@@ -29,6 +29,7 @@ public class SoundSettings : MonoBehaviour
 
     public void SetVolumeFromSlider() {
         SetVolume(soundSlider.value);
+        Debug.Log("UnityDebug Master_Vol_(Slider): " + soundSlider.value);
     }
 
     public void RefreshSlider(float _value) {
@@ -42,7 +43,7 @@ public class SoundSettings : MonoBehaviour
         // SmoothDamp gives eased interpolation toward lastValue
         smoothedValue = Mathf.SmoothDamp(smoothedValue, lastValue, ref velocity, timeToSettle);
 
-        //Debug.Log("UpdateSmooth: " + smoothedValue);
+        //Debug.Log("UnityDebug Master_Volume: " + smoothedValue);
         // Apply to system
         PlayerPrefs.SetFloat("SavedMasterVolume", smoothedValue);
         masterMixer.SetFloat("MasterVolume", Mathf.Log10(smoothedValue / 100) * 20f);
