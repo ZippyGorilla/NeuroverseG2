ea36508ab82d8b4fb5ff3c8bad5fbe815425162b
ZippyGorilla <54118929+ZippyGorilla@users.noreply.github.com>
2025-05-20 17:52:47 +0200

    update questionnaire names

---
+++ Assets/XRI_Examples/Scripts/QuestionnaireScripts/Choice_SoundTexture.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Choice_SoundTexture : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown dropdown;
+
+    SoundSettings sSScript;
+
+    void Start()
+    {
+        sSScript = GameObject.FindGameObjectWithTag("SoundSetSlider").GetComponent<SoundSettings>();
+    }
+
+    public void GetValue()
+    {
+        int pickedEntryIndex = dropdown.value;
+
+        if (dropdown.value == 0)
+        {
+            Debug.Log("UnityDebug Volume_Tolerance: " + "No_option_chosen");
+        }
+        else if (dropdown.value == 1)
+        {
+            Debug.Log("UnityDebug Volume_Tolerance: " + "Option_1");
+            sSScript.QuarterVol();
+        }
+        else if (dropdown.value == 2)
+        {
+            Debug.Log("UnityDebug Volume_Tolerance: " + "Option_2");
+        }
+        else if (dropdown.value == 3)
+        {
+            Debug.Log("UnityDebug Volume_Tolerance: " + "Option_3");
+        }
+        else if (dropdown.value == 4)
+        {
+            Debug.Log("UnityDebug Volume_Tolerance: " + "Option_4");
+        }
+    }
+}
