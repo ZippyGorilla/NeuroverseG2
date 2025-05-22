844e1e344e332a60bde13ac2085027abf92b2ccc
msizavieira <fa66cd@student.aau.dk>
2025-05-22 13:25:28 +0200

    Connecting SoundMenu to Questionnaire

--- Assets/XRI_Examples/Scripts/QuestionnaireScripts/SoundClarity.cs
+++ Assets/XRI_Examples/Scripts/QuestionnaireScripts/SoundClarity.cs
@@ -1,17 +1,55 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundClarity : MonoBehaviour
 {
     [SerializeField] private TMP_Dropdown dropdown;
 
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider[] sfxSliders;
 
+    [SerializeField] GameObject eqHandle;
 
     public void GetValue()
     {
         int pickedEntryIndex = dropdown.value;
 
-        Debug.Log(pickedEntryIndex);
+        if (dropdown.value == 0)
+        {
+            Debug.Log("UnityDebug SoundEnvs: " + "No_option_chosen");
+        }
+        else if (dropdown.value == 1)
+        {
+            Debug.Log("UnityDebug SoundEnvs: " + "Option_1");
+            musicSlider.SetValueWithoutNotify(0.2f);
+            for (int i = 0; i < sfxSliders.Length; i++)
+            {
+                sfxSliders[i].SetValueWithoutNotify(0.35f);
+            }
+        }
+        else if (dropdown.value == 2)
+        {
+            Debug.Log("UnityDebug SoundEnvs: " + "Option_2");
+            musicSlider.SetValueWithoutNotify(0.4f);
+            for (int i = 0; i < sfxSliders.Length; i++)
+            {
+                sfxSliders[i].SetValueWithoutNotify(0.55f);
+            }
+        }
+        else if (dropdown.value == 3)
+        {
+            Debug.Log("UnityDebug SoundEnvs: " + "Option_3");
+            musicSlider.SetValueWithoutNotify(0.6f);
+            for (int i = 0; i < sfxSliders.Length; i++)
+            {
+                sfxSliders[i].SetValueWithoutNotify(0.7f);
+            }
+        }
+        else if (dropdown.value == 4)
+        {
+            Debug.Log("UnityDebug SoundEnvs: " + "Option_4");
+        }
     }
 }
